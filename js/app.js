d11c9e59668d2e4cc446830bbdb230eaa6a9df34
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-08-18 19:36:49 -0400

    working on synth

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-// //create a synth and connect it to the master output (your speakers)
-// var synth = new Tone.Synth().toMaster();
+//create a synth and connect it to the master output (your speakers)
+var synth = new Tone.Synth().toMaster();
 
-// //play a middle 'C' for the duration of an 8th note
-// synth.triggerAttackRelease("C4", "8n");
-// console.log('test');
+function playSound(note) {
+    console.log(note);
+    synth.triggerAttackRelease("C4", "4n");
+  
+}
+
+
