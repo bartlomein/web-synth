eafd5dfd7855b579d87ffd6c55eb0334fe29bc10
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-08-18 20:26:48 -0400

    refactored some basic funtion

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
-//create a synth and connect it to the master output (your speakers)
+//BASIC SYNTH FUNCTIONALITY FOR NOW
 var synth = new Tone.Synth().toMaster();
 
-function playSound(note) {
-    console.log(note);
-    synth.triggerAttackRelease(note+"4", "8n");
+function playSound(note, octave) {
+
+    synth.triggerAttackRelease(note + octave, "8n");
 
 }
-function playSoundOctaveHigher(note) {
-    console.log(note);
-    synth.triggerAttackRelease(note + "5", "8n");
 
-}
 
 
